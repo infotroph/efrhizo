eb5237c6ea947b510a0b3c6555476fd27652a5ee
Chris Black <chris@bodger.org>
2015-03-15 20:46:05 -0500

    overlay all images in path

--- scripts/addpats.sh
+++ scripts/addpats.sh
@@ -6,10 +6,9 @@
 IMGPATH="/Users/chrisb/rdp-tx/ef2012_t1-t53/"
 OUTPATH="/Users/chrisb/UI/efrhizo/tmp/"
 
-FILES=( $(find $IMGPATH -name '*.pat' -print0 | xargs -0 basename -s .pat) )
+FILES=$(find $IMGPATH -name '*.pat' -print0 | xargs -0 basename -s .pat)
 
-# only using first 50 for debugging, can use whole array later
-for F in ${FILES[@]:9:50}; do
+for F in $FILES; do
 	echo $F
 	./scripts/showpat.py "$IMGPATH$F".jpg "$IMGPATH$F".pat "$OUTPATH$F".png
 done
